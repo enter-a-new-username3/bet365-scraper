c115e220d372acce93a56ee78d4ff639a6566dc1
someone <who is this guy@gm.com>
2025-11-18 22:26:39 +0300

    temporary api change

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ s.go_homepage()
 
 
 r = s.protected_get(
-    "https://www.bet365.com/splashcontentapi/getsplashpods?lid=32&zid=0&pd=%23AS%23B1%23&cid=198&cgid=3&ctid=198&csid=3&tzo=-300",
+    "https://www.bet365.com/matchbettingcontentapi/coupon?lid=1&zid=9&pd=%23AC%23B1%23C1%23D8%23E183853589%23F3%23G40%23&cid=197&cgid=82&ctid=13",
     headers={
         "User-Agent": "Mozilla (Linux; Android 12 Phone; CPU M2003J15SC OS 12 like Gecko) Chrome/141.0.7390.122 Gen6 bet365/8.0.14.00",
         "X-b365App-ID": "8.0.14.00-row",
@@ -34,3 +34,4 @@ for a, i in enumerate(pattern.finditer(r.text)):
     if a == 10:
         print("\t...")
         break
+print(r.text)
