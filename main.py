4bb2365ac510f7bc6f11fa2ab79ce35766eacd50
someone <who is this guy@gm.com>
2025-12-09 15:19:24 +0300

    simple

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ s.go_homepage()
 
 
 r = s.protected_get(
-    "https://www.bet365.com/splashcontentapi/getsplashpods?lid=32&zid=0&pd=%23AS%23B1%23&cid=198&cgid=3&ctid=198&csid=3&tzo=-300",
+    "https://www.bet365.com/matchbettingcontentapi/coupon?lid=1&zid=1&pd=%23AC%23B1%23C1%23D8%23E184563985%23F3%23I4%23P30261%23&cid=197&cgid=2&ctid=197",
     headers={
         "User-Agent": "Mozilla (Linux; Android 12 Phone; CPU M2003J15SC OS 12 like Gecko) Chrome/141.0.7390.122 Gen6 bet365/8.0.14.00",
         "X-b365App-ID": "8.0.14.00-row",
